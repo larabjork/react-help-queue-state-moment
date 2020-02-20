3160fdf5a8ef433ab736f0cf463a252db4e6058f
Student <example@example.com>
2020-02-20 10:21:54 -0800

    add Wednesday homework

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,74 +4,92 @@ import TicketList from './TicketList';
 import NewTicketControl from './NewTicketControl';
 import { Switch, Route } from 'react-router-dom';
 import Moment from 'moment';
+import Admin from './Admin';
+import Error404 from './Error404';
+import { v4 } from 'uuid';
 
 class App extends React.Component {
 
   constructor(props) {
-  super(props);
-  this.state = {
-    masterTicketList: []
-  };
-  this.handleAddingNewTicketToList = this.handleAddingNewTicketToList.bind(this);
-}
+    super(props);
+    this.state = {
+      masterTicketList: {},
+      selectedTicket: null
+    };
+    this.handleAddingNewTicketToList = this.handleAddingNewTicketToList.bind(this);
+    this.handleChangingSelectedTicket = this.handleChangingSelectedTicket.bind(this);
+  }
 
-componentDidMount() {
-  console.log('componentDidMount');
-  this.waitTimeUpdateTimer = setInterval(() =>
-    this.updateTicketElapsedWaitTime(),
-  60000
-  );
-}
+  componentDidMount() {
+    console.log('componentDidMount');
+    this.waitTimeUpdateTimer = setInterval(() =>
+      this.updateTicketElapsedWaitTime(),
+    60000
+    );
+  }
 
-componentWillUnmount(){
-  console.log('componentWillUnmount');
-  clearInterval(this.waitTimeUpdateTimer);
-}
+  componentWillUnmount(){
+    console.log('componentWillUnmount');
+    clearInterval(this.waitTimeUpdateTimer);
+  }
 
-componentWillMount() {
-  console.log('componentWillMount');
-}
+  componentWillMount() {
+    console.log('componentWillMount');
+  }
 
-componentWillReceiveProps() {
-  console.log('componentWillReceiveProps');
-}
+  componentWillReceiveProps() {
+    console.log('componentWillReceiveProps');
+  }
 
-shouldComponentUpdate() {
-  console.log('shouldComponentUpdate');
-  return true;
-}
+  shouldComponentUpdate() {
+    console.log('shouldComponentUpdate');
+    return true;
+  }
 
-componentWillUpdate() {
-  console.log('componentWillUpdate');
-}
+  componentWillUpdate() {
+    console.log('componentWillUpdate');
+  }
 
-componentDidUpdate() {
-  console.log('componentDidUpdate');
-}
+  componentDidUpdate() {
+    console.log('componentDidUpdate');
+    console.log(this.state);
+  }
 
-updateTicketElapsedWaitTime() {
-  let newMasterTicketList = this.state.masterTicketList.slice();
-  newMasterTicketList.forEach((ticket) =>
-    ticket.formattedWaitTime = (ticket.timeOpen).fromNow(true)
-  );
-  this.setState({masterTicketList: newMasterTicketList});
-}
+  updateTicketElapsedWaitTime() {
+    var newMasterTicketList = Object.assign({}, this.state.masterTicketList);
+    Object.keys(newMasterTicketList).forEach(ticketId => {
+      newMasterTicketList[ticketId].formattedWaitTime = (newMasterTicketList[ticketId].timeOpen).fromNow(true);
+    });
+    this.setState({masterTicketList: newMasterTicketList});
+  }
 
-handleAddingNewTicketToList(newTicket){
-  var newMasterTicketList = this.state.masterTicketList.slice();
-  newTicket.formattedWaitTime = (newTicket.timeOpen).fromNow(true);
-  newMasterTicketList.push(newTicket);
-  this.setState({masterTicketList: newMasterTicketList});
-}
+  handleAddingNewTicketToList(newTicket){
+    var newTicketId = v4();
+    var newMasterTicketList = Object.assign({}, this.state.masterTicketList, {
+      [newTicketId]: newTicket
+    });
+    newMasterTicketList[newTicketId].formattedWaitTime = newMasterTicketList[newTicketId].timeOpen.fromNow(true);
+    this.setState({masterTicketList: newMasterTicketList});
+  }
+
+
+  handleChangingSelectedTicket(ticket){
+    this.setState({selectedTicket: ticket});
+  }
 
 
   render(){
+    console.log(this.state.masterTicketList);
     return (
       <div>
         <Header/>
         <Switch>
           <Route exact path='/' render={()=><TicketList ticketList={this.state.masterTicketList} />} />
           <Route path='/newticket' render={()=><NewTicketControl onNewTicketCreation={this.handleAddingNewTicketToList} />} />
+          <Route path='/admin' render={(props)=><Admin ticketList={this.state.masterTicketList} currentRouterPath={props.location.pathname}
+          onTicketSelection={this.handleChangingSelectedTicket}
+          selectedTicket={this.state.selectedTicket}/>} />
+          <Route component={Error404} />
         </Switch>
       </div>
     );
